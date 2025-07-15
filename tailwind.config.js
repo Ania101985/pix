5cd9e8b8b0959d9e58ad44325984311c28c530db
Ania101985 <aaltyn@gmail.com>
2025-07-15 11:21:24 +0200

    Primer commit de Pix Adventures

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        fredoka: ['Fredoka', 'sans-serif'],
-        baloo: ['"Baloo 2"', 'cursive'],
-        nunito: ['Nunito', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
