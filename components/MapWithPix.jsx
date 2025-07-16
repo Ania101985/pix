cfd29bc99710a68bf4f499bd11c27fba74780034
Ania101985 <aaltyn@gmail.com>
2025-07-16 22:19:19 +0200

    Update Mobile mapa

--- components/MapWithPix.jsx
+++ components/MapWithPix.jsx
@@ -2,62 +2,28 @@
 import Image from 'next/image';
 
 const pixItems = [
-  {
-    name: 'SCRATCHJR',
-    img: '/images/pixlabel2/pixlabel1.png',
-    style: { top: '79%', left: '35%' },
-  },
-  {
-    name: 'SCRATCH INTERMEDIO',
-    img: '/images/pixlabel2/pixlabel2.png',
-    style: { top: '57%', left: '57%' },
-  },
-  {
-    name: 'ALFABETIZACIÓN DIGITAL',
-    img: '/images/pixlabel2/pixlabel3.png',
-    style: { top: '82%', left: '55%' },
-  },
-  {
-    name: 'MAKECODE ARCADE',
-    img: '/images/pixlabel2/pixlabel4.png',
-    style: { top: '16%', left: '62%' },
-  },
-  {
-    name: 'APP INVENTOR',
-    img: '/images/pixlabel2/pixlabel5.png',
-    style: { top: '40%', left: '83%' },
-  },
-  {
-    name: 'HTML & CSS',
-    img: '/images/pixlabel2/pixlabel6.png',
-    style: { top: '30%', left: '32%' },
-  },
-  {
-    name: 'PYTHON',
-    img: '/images/pixlabel2/pixlabel7.png',
-    style: { top: '72%', left: '75%' },
-  },
-  {
-    name: 'JAVASCRIPT',
-    img: '/images/pixlabel2/pixlabel8.png',
-    style: { top: '40%', left: '13%' },
-  },
-  {
-    name: 'SCRATCH BÁSICO',
-    img: '/images/pixlabel2/pixlabel9.png',
-    style: { top: '49%', left: '39%' },
-  },
+  { name: 'SCRATCHJR', img: '/images/pixlabel2/pixlabel1.png', style: { top: '79%', left: '35%' } },
+  { name: 'SCRATCH INTERMEDIO', img: '/images/pixlabel2/pixlabel2.png', style: { top: '57%', left: '57%' } },
+  { name: 'ALFABETIZACIÓN DIGITAL', img: '/images/pixlabel2/pixlabel3.png', style: { top: '82%', left: '55%' } },
+  { name: 'MAKECODE ARCADE', img: '/images/pixlabel2/pixlabel4.png', style: { top: '16%', left: '62%' } },
+  { name: 'APP INVENTOR', img: '/images/pixlabel2/pixlabel5.png', style: { top: '40%', left: '83%' } },
+  { name: 'HTML & CSS', img: '/images/pixlabel2/pixlabel6.png', style: { top: '30%', left: '32%' } },
+  { name: 'PYTHON', img: '/images/pixlabel2/pixlabel7.png', style: { top: '72%', left: '75%' } },
+  { name: 'JAVASCRIPT', img: '/images/pixlabel2/pixlabel8.png', style: { top: '40%', left: '13%' } },
+  { name: 'SCRATCH BÁSICO', img: '/images/pixlabel2/pixlabel9.png', style: { top: '49%', left: '39%' } },
 ];
 
 export default function MapWithPix() {
   return (
     <section className="relative w-full max-w-5xl mx-auto aspect-[3/4] sm:aspect-[16/9] my-12">
+      {/* Map image */}
       <img
         src="/map2aa.png"
         alt="Mapa Pix"
-        className="absolute inset-0 w-full h-full object-cover z-0"
+        className="absolute inset-0 w-full h-full object-fill z-0"
       />
 
+      {/* Labels */}
       {pixItems.map((pix, index) => (
         <div
           key={index}
